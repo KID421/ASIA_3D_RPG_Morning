03c6857fc2f2993823ce5f57c0b096a254a80f05
KID <50666207+KID421@users.noreply.github.com>
2019-12-25 10:59:38 +0800

    顯示勝利提示

---
+++ UnityProject/Assets/Scripts/EatProp.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI; // 引用 介面 API
+
+public class EatProp : MonoBehaviour
+{
+    [Header("寶箱數量介面")]
+    public Text textCount;
+    [Header("提示畫面")]
+    public GameObject objTip;
+
+    public int propCount;       // 目前寶箱數量
+    public int propTotal = 10;  // 寶箱總數
+
+    public bool finish;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "寶箱")
+        {
+            Destroy(collision.gameObject);
+
+            propCount++;                                            // 遞增
+            textCount.text = "寶箱：" + propCount + " / 10";        // 更新介面
+
+            // 如果 目前數量 等於 總數 顯示 勝利提示
+            if (propCount == propTotal)
+            {
+                finish = true;
+                objTip.SetActive(true);
+            }
+        }
+    }
+}
